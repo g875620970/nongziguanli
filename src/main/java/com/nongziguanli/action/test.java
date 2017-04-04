019c6263e152e03c573f42e7ae176ade3347ed94
g875620970 <875620970@qq>
2017-04-04 20:20:57 +0800

    s

--- src/main/java/com/nongziguanli/action/test.java
+++ src/main/java/com/nongziguanli/action/test.java
@@ -1,10 +1,12 @@
 package com.nongziguanli.action;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class test {
 	
+	@RequestMapping("/test")
 	public void test(){
 		
 	}
